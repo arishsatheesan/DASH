c941b016800195a38261266290e3f0af709c2f98
Arish S <38071757+arishsu@users.noreply.github.com>
2020-11-09 18:10:56 +0100

    wip

---
+++ Python files/LeNet_5_py.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 17:46:58 2019
+
+@author: u0129240
+"""
+
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+from contextlib import redirect_stdout
+#import cv2
+
+batch_size = 128
+num_classes = 10
+epochs = 2
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+    
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model=Sequential()
+model.add(Conv2D(6, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),strides=2))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(156))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+model.layers
+model.summary()
+model.save('lenet5.h5')
+
+with open('modelsummary.txt', 'w') as f:
+    with redirect_stdout(f):
+        model.layers
+        model.summary()
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
---
+++ Python files/display_resource.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 19:18:23 2019
+
+@author: u0129240
+"""
+
+f = open('resource_lenet1_pipelined.txt','r')
+message = f.read()
+print(message)
+f.close()
+print("Press Enter to continue ...")
+input() 
